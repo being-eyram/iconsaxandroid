9a60c097684818ce6dff44ab400991b846a343bd
Eyram Michael <Eyram.Hlorgbe@Hubtel.com>
2022-12-30 07:45:56 +0000

    Reference drawable resources from IconsSax file

---
+++ iconsax/src/main/java/io/eyram/iconsax/Iconsax.kt
@@ -0,0 +1,11 @@
+package io.eyram.iconsax
+
+object IconSax {
+
+    val Bold : BoldIcons = BoldIcons
+    val Broken : BrokenIcons = BrokenIcons
+    val Bulk : BulkIcons = BulkIcons
+    val Linear : LinearIcons = LinearIcons
+    val Outline : OutlineIcons = OutlineIcons
+    val TwoTone : TwoToneIcons = TwoToneIcons
+}
